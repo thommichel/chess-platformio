c1d093d57d834bfadbb83f9e314b87efc8150427
ThomasMichel1122334455 <michel.t@northeastern.edu>
2025-03-07 15:27:35 -0500

    fixed example python script

--- serial_comm.py
+++ serial_comm.py
@@ -4,7 +4,7 @@ import time
 import struct
 
 def ser_print(data):
-    print(data, end='')
+    print(data.decode(), end='')
 
 def identify_esp():
     ports = serial.tools.list_ports.comports()
@@ -19,31 +19,26 @@ def send_command(device: Serial, cmd:int, type:int, p1:float, p2:float):
     p1_b = struct.pack(">f", p1)
     p2_b = struct.pack(">f", p2)
     arrr = bytearray(cmd_b + type_b + p1_b + p2_b)
-    print(rf'{arrr}')
     device.write(arrr)
 
-with Serial(identify_esp(), 115200) as esp:
-    try:
+try:
+    with Serial(identify_esp(), 115200) as esp:
         print('wait for ESP to boot...')
-        time.sleep(5)
+        # time.sleep(5)
         print('done waiting')
         while esp.in_waiting > 0:
             resp = esp.readline()
-            ser_print(resp.decode())
+            ser_print(resp)
         while True:
-            input('enter anything: ')
-            if True:
-                print("attempting to send command")
+            a = input('enter anything: ')
+            if a == "0":
                 send_command(esp, 0, 1, 500, 0)
-                time.sleep(0.05)
-                if esp.in_waiting > 0:
-                    resp = esp.readline()
-                    print("REPOSNSE: ", end = '')
-                    print(resp, end='')
-                else:
-                    print("error: did not receive a response, but should've")
-
-    except KeyboardInterrupt:
-        esp.close()
-        raise KeyError()
+            else:
+                send_command(esp, 2, 1, 500, 0)
+            time.sleep(0.05)
+            while esp.in_waiting > 0:
+                resp = esp.readline()
+                ser_print(resp)
+except KeyboardInterrupt:
+    print("\n\nclosing...")
     
